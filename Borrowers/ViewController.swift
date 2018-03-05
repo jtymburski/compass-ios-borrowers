b4089f8197e0d0f02f18943ca745288746428aba
Jordan Tymburski <hello@jordantymburski.com>
2018-03-05 10:45:57 -0700

    Initial Commit

---
+++ Borrowers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Borrowers
+//
+//  Created by Kevin Smith on 2018-03-05.
+//  Copyright © 2018 GN Compass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
